96e11cde61b0bc6767695b7d6494271392ffbd45
André Maree <ammaree@users.noreply.github.com>
2022-09-17 12:29:52 +0200

    Update syslog.c

--- syslog.c
+++ syslog.c
@@ -148,8 +148,7 @@ static int IRAM_ATTR xSyslogConnect(void) {
 	sCtx.sa_in.sin_port = htons(IP_PORT_SYSLOG_UDP);
 	sCtx.type = SOCK_DGRAM;
 	sCtx.flags = SO_REUSEADDR;
-	sCtx.d_flags = 0;
-	sCtx.d_ndebug = 1;				// disable debug in socketsX.c
+	sCtx.d = (netx_dbg_t) { .sl = 1 };
 	int	iRV = xNetOpen(&sCtx);
 	if (iRV >= erSUCCESS) {
 		if (xNetSetRecvTO(&sCtx, flagXNET_NONBLOCK) >= erSUCCESS) {
