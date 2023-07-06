b7051fbafee414834e7e567424148084fc7da36f
André Maree <ammaree@users.noreply.github.com>
2023-07-06 13:49:18 +0200

    Update syslog.h

--- syslog.h
+++ syslog.h
@@ -61,7 +61,7 @@ extern "C" {
 #define	SL_LEVEL					(CONFIG_LOG_MAXIMUM_LEVEL + 2)
 #define	SL_LEV_DEF					(CONFIG_LOG_DEFAULT_LEVEL + 2)
 
-#define SL_LOG(x, f, ...) do{ if (SL_LEVEL>=(x)) vSyslog(x,__FUNCTION__,f,##__VA_ARGS__); } while(0)
+#define SL_LOG(x, f, ...) do{ if ((x) <= SL_LEVEL) vSyslog(x,__FUNCTION__,f,##__VA_ARGS__); } while(0)
 
 #define	SL_EMER(f, ...)				SL_LOG(SL_SEV_EMERGENCY, f, ##__VA_ARGS__)
 #define	SL_ALRT(f, ...)				SL_LOG(SL_SEV_ALERT, f, ##__VA_ARGS__)
